a06210f913fbf6b4533424e519323c169f586090
sebastianscheibe <root@techstack.de>
2018-08-05 23:09:22 +0200

    Another commit

--- src/pages/ProjectPage/index.js
+++ src/pages/ProjectPage/index.js
@@ -25,7 +25,7 @@ export default class ProjectPage extends React.Component {
                 <div className="content-grid">
                     <h1 style={{color: ProjectStyles.colorPrimary}}>Projects</h1>
                     <ul style={{color: ProjectStyles.colorPrimary}}>
-                        {this.projects.map(project => {
+                        {this.projects.filter(project => project.hide !== true).map(project => {
                             return <li>
                                 <div className="project-content">
                                     <ProjectEntry title={project.title} description={project.description}
